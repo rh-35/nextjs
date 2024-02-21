79d67d2f6627171bb8955574952e1f82a1c11750
rh-35 <rheamilberg@gmail.com>
2024-02-20 16:52:38 -0800

    ok

--- app/listening/page.tsx
+++ app/listening/page.tsx
@@ -1,7 +1,7 @@
-export default function listening(){
-    return(
+export default function listening() {
+    return (
         <div>
             music stats will go here
         </div>
-    )
+    );
 }
