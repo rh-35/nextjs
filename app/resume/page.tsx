bc9dcb51b1a32f836d395612d3d4e1afdd88b70a
rh-35 <rheamilberg@gmail.com>
2024-02-20 20:19:06 -0800

    okay

---
+++ app/resume/page.tsx
@@ -0,0 +1,7 @@
+export default function resume() {
+    return(
+        <p>
+            resume
+        </p>
+    )
+}
--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -3,11 +3,26 @@
 export function Navbar(){
     return(
         <div>
-            <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+            <nav className=" dark:bg-zinc-800">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <img src="images/rhea.png" className="h-8" alt="Icon"></img>
+                        <img src="/images/rhea.png" className="h-8 rounded-full" alt="Icon"/>
+                        <span className="self-center text-2xl font-semibold">Rhea Milberg</span>
                     </a>
+                <div className="hidden w-full md:block md:w-auto">
+                    <ul className="font-medium flex flex-auto space-x-8">
+                        <li>
+                            <a href="/about" className="hover:text-rose-800">
+                               About Me 
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/resume" className="hover:text-rose-800">
+                                Resume
+                            </a>
+                        </li>
+                    </ul>
+                </div>
                 </div>  
             </nav>
         </div>
