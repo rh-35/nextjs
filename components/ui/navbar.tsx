09ddfe476937a80945519518cd4488205edbd025
rh-35 <rheamilberg@gmail.com>
2024-02-20 17:59:28 -0800

    ok

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -3,9 +3,11 @@
 export function Navbar(){
     return(
         <div>
-            <nav>
+            <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    
+                    <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                        <img src="images/rhea.png" className="h-8" alt="Icon"></img>
+                    </a>
                 </div>  
             </nav>
         </div>
