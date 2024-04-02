bd2447d1608b59a4b72b77ed6391bcae6258b3b9
Rhea Milberg <147758777+rh-35@users.noreply.github.com>
2024-04-02 15:54:24 +0000

    kill me

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex-col justify-center content-center">
-      <div className="text-center flex p-2 space-x-2 m-24 items-end border">
-        <div className="text-6xl font-semibold">Hello</div>
-        <div className="text-3xl">I'm</div>
-        <div className="text-6xl font-semibold">Rhea</div>
+    <div className="flex justify-center content-center">
+      <div className="flex-col text-center flex m-24">
+        <div className="text-5xl font-semibold">Hello I'm Rhea</div>
         <div className="text-3xl">a dumb idiot that does stupid things</div>
       </div>
     </div>
