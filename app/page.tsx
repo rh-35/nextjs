ebd3c6ee8441a8f2419680d0f073779656a6d3c5
rh-35 <rheamilberg@gmail.com>
2024-04-01 22:20:06 -0700

    fuck my life

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <div className="flex justify-center">
-      <div className="text-center m-24 text-2xl">Home Page</div>
+    <div className="flex-col justify-center content-center">
+      <div className="text-center flex p-2 space-x-2 m-24 items-end border">
+        <div className="text-6xl font-semibold">Hello</div>
+        <div className="text-3xl">I'm</div>
+        <div className="text-6xl font-semibold">Rhea</div>
+        <div className="text-3xl">a dumb idiot that does stupid things</div>
+      </div>
     </div>
   );
 }
