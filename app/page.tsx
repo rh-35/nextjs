6e213dc369d2254df9d1f50034b23ca781c09b9c
rh-35 <rheamilberg@gmail.com>
2024-04-05 23:49:49 -0700

    fml

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
     <div className="w-screen overflow-x-hidden mt-48">  
         <div className="flex-col text-center">
           <div className="text-5xl font-semibold">Hello I&apos;m Rhea</div>
-          <div className="text-3xl opacity-50">a dumb idiot that does stupid things</div>
+          <div className="text-3xl text-zinc-400">a dumb idiot that does stupid things</div>
         </div>
       <div className="mt-32">
         <p className="text-3xl ml-10 font-semibold">My &quot;Achievements&quot;</p>
-        <p className="pl-10 opacity-50">Things I make money from (or not)</p>
+        <p className="pl-10 text-zinc-400">Things I make money from (or not)</p>
         <Work/>
       </div>
     </div>
--- components/work.tsx
+++ components/work.tsx
@@ -12,26 +12,26 @@ export function Work() {
             key={workItem.title}
             className="project relative rounded-lg bg-zinc-900"
           >
-            <div className="project-content">
+            <div className="">
               <div className="flex h-full flex-col justify-between px-5 py-4">
                 <div>
                   <h3 className={`mb-3 text-2xl font-semibold`}>
                     {workItem.title}
                   </h3>
                   <p
-                    className={`m-0 text-sm opacity-50 flex items-start space-x-2`}
+                    className={`m-0 text-sm text-zinc-400 flex items-start space-x-2`}
                   >
                     <span>{workItem.where}</span>
                   </p>
 
                   <p
-                    className={`m-0 text-sm opacity-50 flex items-start space-x-2`}
+                    className={`m-0 text-sm text-zinc-400 flex items-start space-x-2`}
                   >
                     <span>{workItem.when}</span>
                   </p>
 
                   <p
-                    className={`m-0 text-sm opacity-50 flex items-start space-x-2`}
+                    className={`m-0 text-sm text-zinc-400 flex items-start space-x-2`}
                   >
                     <span>{workItem.about}</span>
                   </p>
