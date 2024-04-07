14a8b6d2a15bd88f19722d69d4d20bf026495dc1
rh-35 <rheamilberg@gmail.com>
2024-04-06 21:40:51 -0700

    fml

--- app/showcase/page.tsx
+++ app/showcase/page.tsx
@@ -4,7 +4,7 @@ export default function showcase() {
   return(
     <div className="mt-32">
         <p className="text-3xl ml-10 font-semibold">Gallery Time</p>
-        <p className="pl-10 text-zinc-400">Good (and bad) Photos I've Taken</p>
+        <p className="pl-10 text-zinc-400">Good (and bad) Photos I&apos;ve Taken</p>
       <Showcase/>
     </div>
   )}
