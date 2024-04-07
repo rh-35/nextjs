b3b5d9fd8ad13081d493bc8f26068b3d4d20a01b
rh-35 <rheamilberg@gmail.com>
2024-04-06 23:49:09 -0700

    fml

--- components/showcase.tsx
+++ components/showcase.tsx
@@ -6,16 +6,17 @@ import { showcase } from "@/lib/showcase";
 
 export function Showcase() {
     return(
-        <div className="flex w-full h-full flex-wrap justify-center my-20">
+        <div className="flex justify-center w-full h-full flex-wrap my-20">
             {showcase.map((showcaseItem) => (
                 <div key={showcaseItem.staticPhoto} className="relative rounded-lg">
-                    <div className="flex h-full flex-col justify-between px-2 py-2">
+                    <div className="flex h-full flex-col justify-normal px-2 py-2">
                         <div>
                             <Image
                                 className="rounded-2xl"
                                 src={showcaseItem.path}
-                                height={500}
-                                width={500}
+                                quality={100}
+                                height={0}
+                                width={750}
                                 title={showcaseItem.staticPhoto}
                                 alt={showcaseItem.staticPhoto}
                             />
