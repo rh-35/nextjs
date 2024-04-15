86cec2c17e27959cf5819c5366ba0a8f8c00e231
rh-35 <rheamilberg@gmail.com>
2024-04-14 23:20:09 -0700

    fml

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,9 +3,9 @@ import { Liveskills } from "@/components/liveskills";
 
 export default function Page() {
   return (
-    <div className="mt-56 mx-8 md:mx-10 xl:mx-56">
+    <div className="mt-56 mx-10 md:mx-10 xl:mx-56">
         <div className="flex-col items-center space-x-2 justify-center text-center">
-            <div className="text-5xl font-bold">Just your stereotypical<div className="bg-gradient-to-r from-blue-400 to-pink-400 inline-block text-transparent bg-clip-text text-5xl font-bold px-2">Transfem</div></div>
+            <div className="text-5xl font-bold">Just your typical<div className="bg-gradient-to-r from-blue-400 to-pink-400 inline-block text-transparent bg-clip-text text-5xl font-bold px-2">Transfem</div></div>
             <div className="text-4xl font-semibold text-zinc-400">Technical Theater and Sound Nerd</div>
         </div>
         <div className="w-full h-8"/>
