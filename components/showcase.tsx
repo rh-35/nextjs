82f734cd19bc5064260e31d5443a128a3b5d095d
Rhea Milberg <147758777+rh-35@users.noreply.github.com>
2024-04-30 20:50:13 +0000

    fml

--- components/showcase.tsx
+++ components/showcase.tsx
@@ -10,32 +10,15 @@ export function Showcase() {
   const secondHalf = showcase.slice(half);
   return (
     <div>
-      <div className="md:hidden">
-        <div className="flex-col justify-center content-start gap-4">
-          {showcase.map((showcaseItem) => (
-            <div key={showcaseItem.title}>
-              <Image
-                className="rounded-2xl my-6"
-                src={showcaseItem.path}
-                quality={100}
-                height={0}
-                width={1000}
-                title={showcaseItem.title}
-                alt={showcaseItem.title}
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="hidden md:block">
-        <div className="flex items-start justify-center space-x-6 mt-10 mx-10">
-          <div className="flex-col justify-center content-start gap-4">
+        <div className="flex items-start justify-center space-x-2 md:space-x-6 mt-10">
+          <div className="flex-col justify-center content-start">
             {firstHalf.map((showcaseItem) => (
               <div key={showcaseItem.title}>
                 <Image
-                  className="rounded-2xl mt-6"
+                  className="rounded-md md:rounded-2xl mt-3 md:mt-6"
                   src={showcaseItem.path}
                   quality={100}
+                  placeholder="blur"
                   height={0}
                   width={1080}
                   title={showcaseItem.title}
@@ -48,10 +31,11 @@ export function Showcase() {
             {secondHalf.map((showcaseItem) => (
               <div key={showcaseItem.title}>
                 <Image
-                  className="rounded-2xl mt-6"
+                  className="rounded-md md:rounded-2xl mt-3 md:mt-6"
                   src={showcaseItem.path}
                   quality={100}
                   height={0}
+                  placeholder="blur"
                   width={1080}
                   title={showcaseItem.title}
                   alt={showcaseItem.title}
@@ -61,6 +45,5 @@ export function Showcase() {
           </div>
         </div>
       </div>
-    </div>
   );
 }
